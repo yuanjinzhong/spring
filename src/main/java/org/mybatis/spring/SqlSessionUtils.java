630cd894899b60706c1d2a2a6d75f51ccec06d15
jinzhong.yuan <jinzhong.yuan@huolala.cn>
2023-04-23 20:26:17 +0800

    1

--- src/main/java/org/mybatis/spring/SqlSessionUtils.java
+++ src/main/java/org/mybatis/spring/SqlSessionUtils.java
@@ -110,11 +110,11 @@ public final class SqlSessionUtils {
     LOGGER.debug(() -> "Creating a new SqlSession");
     session = sessionFactory.openSession(executorType);
 
-<<<<<<< HEAD
+
     //获取session后，和当前事务绑定起来？？
-=======
+
     //新创建的sqlSession和资源管理器绑定
->>>>>>> 78bf9e63 (加点注射)
+
     registerSessionHolder(sessionFactory, executorType, exceptionTranslator, session);
 
     return session;
